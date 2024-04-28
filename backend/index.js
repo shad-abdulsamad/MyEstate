be4fec47d9bf9344bbc08acf160adcc18fdb2043
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-04-28 20:38:42 +0300

    created the signup functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,8 +11,10 @@ mongoose.connect('mongodb://localhost/myestate')
 .catch(()=>console.log('Could not connect to the database'));
 
 const app = express();
+app.use(express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
