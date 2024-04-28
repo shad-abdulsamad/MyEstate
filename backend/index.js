8b3f9a9a45b5bded04c540c068a92800dee6886b
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-04-28 17:58:55 +0300

    connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+mongoose.connect('mongodb://localhost/estate')
+.then(()=>console.log('Connected to the database'))
+.catch(()=>console.log('Could not connect to the database'));
 
 const app = express();
 
