7930bb855ec0aaec79528ec7e4badd959f1e7907
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-05-01 14:25:01 +0300

    create update user API route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,30 @@
+import bcrpyt from "bcryptjs";
+
+import User from "../models/user.model.js";
+
 export const test = (req,res)=>{
     res.json({
         message:"Hello There"
     });
+}
+
+export const updateUser = async (req,res,next)=>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401,"You can only update your own accoutn"));
+    try {
+        if(req.body.password){
+            req.body.password = bcrpyt.hashSync(req.body.password,10);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set:{
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password,
+                avatar:req.body.avatar
+            }
+        }, {new:true});
+        const {password, ...rest} = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 
@@ -12,6 +14,7 @@ mongoose.connect('mongodb://localhost/myestate')
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
