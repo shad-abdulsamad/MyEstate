bd85817c0416b340050c0fecf9b99bbcc44c336b
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-05-06 15:35:19 +0300

    get user listings api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
  import express from "express";
- import { test, updateUser,deleteUser } from "../controllers/user.controller.js";
+ import { test, updateUser,deleteUser, getUserListings } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
  const router = express.Router();
 
  router.get("/test", test);
  router.post("/update/:id",verifyUser ,updateUser);
  router.delete("/delete/:id",verifyUser ,deleteUser);
+ router.get("/listing/:id",verifyUser,getUserListings);
  export default router;
