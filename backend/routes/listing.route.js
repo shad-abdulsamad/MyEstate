5ae544de4219b5e645f714f917ad48f893b09c0f
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-05-06 17:40:36 +0300

    delete listing

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing,deleteListing } from "../controllers/listing.controller.js";
 import { verifyUser as verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create",verifyToken,createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 
 export default router;
