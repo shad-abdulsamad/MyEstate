7f610ba4883c0d1dc33ca35b714f2211407abc5a
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-05-07 20:43:06 +0300

    minor issues

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import ListingItem from '../components/ListingItem';
 
 export default function Home() {
@@ -55,7 +55,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          MyEstate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
